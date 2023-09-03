2a630124f206a2ba26af264d27948d5921a613d5
Moises Contreras <moises.xiaomi@gmail.com>
2023-09-03 13:16:25 -0300

    Inicio movimiento
    Se empezó a trabajar el movimiento del personaje

--- js/script.js
+++ js/script.js
@@ -14,11 +14,54 @@ const fondoLevel1 = new Image();
 fondoLevel1.src = './img/azukaprueba.jpg';
 
 const imgPlayer = new Image();
-imgPlayer.src = './img/playerAbajo.png'; 
+imgPlayer.src = './img/playerArriba.png'; 
 
-fondoLevel1.onload = () => {
-    context2D.drawImage(fondoLevel1, 0, 0);
-    context2D.drawImage(imgPlayer, canvas.width / 2 - imgPlayer.width / 2, canvas.height / 2 - imgPlayer.height / 2);
+class Sprite {
+    constructor({ position, velocity }){
+        this.position = position
+    }
 }
+
+const background = new scrit({
+    position: {
+        x: 0,
+        y: 0
+    }
+})
+
+function animate() {
+    window.requestAnimationFrame(animate)
+    context2D.drawImage(fondoLevel1, 0, 0);
+    context2D.drawImage(
+        imgPlayer,
+        0,
+        0,
+        imgPlayer.width / 4,
+        imgPlayer.height,
+        canvas.width / 2 - imgPlayer.width / 4 / 2,
+        canvas.height / 2 - imgPlayer.height / 2,
+        imgPlayer.width / 4,
+        imgPlayer.height
+        )   
+}
+
+animate()
+
+window.addEventListener('keydown', (keyMoving) => {
+    switch (keyMoving.key ){
+        case 'w':
+            console.log('la W esta presionada');
+            break
+        case 'a':
+            console.log('la A esta presionada');
+            break
+        case 's':
+            console.log('la S esta presionada');
+            break
+        case 'd':
+            console.log('la D esta presionada');
+            break
+    }
+});
 
 
