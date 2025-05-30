623f44f2bc8a3c01eb3499c3a44c204055f96f1e
comicway <moises.xiaomi@gmail.com>
2025-05-30 19:02:35 -0400

    recuperacion lograda con exito yehh

--- Assets/Scripts/MovingPerso.cs
+++ Assets/Scripts/MovingPerso.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class MovingPerso : MonoBehaviour
 {
-    public float speed = 5.0f; // Variable para controlar la velocidad del personaje
+    public float speed = 5.0f;
 
     void Start()
     {
@@ -11,14 +11,11 @@ public class MovingPerso : MonoBehaviour
 
     void Update()
     {
-        // Obtener la entrada del teclado
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
 
-        // Crear un vector de movimiento
         Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
 
-        // Mover el personaje con la velocidad ajustada
         transform.Translate(movement * speed * Time.deltaTime);
     }
 }
